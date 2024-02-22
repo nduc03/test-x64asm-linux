dc0bd84d725f5504acbd33e75eb8698fd1d5541f
nduc <48994241+nduc03@users.noreply.github.com>
2024-02-22 10:57:43 +0700

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-nasm -f elf64 fizzbuzz.asm -o a64.o
-gcc -m64 -nostdlib -static -o a64.out a64.o
-./a64.out
+filename="file.asm"
+read -p "Enter file name with extension: " filename
+
+if [ -e "$filename" ]; then
+    nasm -f elf64 $filename -o a64.o
+    gcc -m64 -nostdlib -static -o a64.out a64.o
+    ./a64.out
+    rm ./a64.o
+    rm ./a64.out
+else
+    echo "File does not exist."
+fi
